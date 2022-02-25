c4fa25430bdf5cc18436981c2ff062bd6905a2e3
Nragis <Quinn_murphey@me.com>
2022-02-25 07:35:21 -0600

    Finished daily and weekly half pages and got signatures working

---
+++ quarterPages.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+
+def printYear(output, currDate):
+    pass
+
+def printQuarter(output, currDate):
+    pass
+
+def printMonth(output, currDate):
+    pass
+
+def printWeek(output, currDate):
+    pass
+
+def printDay(output, currDate):
+    pass
